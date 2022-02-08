6a319cdfb06d0cdcbc26a750cc7dfbdd9e3ff788
Israel Sanchez <Israel.pro1994@gmail.com>
2022-02-07 23:31:19 -0600

    working on assignment

---
+++ retailer/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Purchase from './Components/Purchase';
+
+function App() {
+  return (
+    <div className="App">
+        <Purchase></Purchase>
+    </div>
+  );
+}
+
+export default App;
---
+++ retailer/src/Components/Purchase.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+ const  purchase = () => {
+
+    // work here
+
+    return(
+        <div>
+            <h1>Hello Wold my guy</h1>
+        </div>
+    );
+
+};
+
+export default purchase;
